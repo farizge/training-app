a1c81b7245a62ff1c06602c55c08d12e18014564
farizge <farizghani11@gmail.com>
2023-08-30 21:36:29 +0700

    remove import

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,11 +5,3 @@
     <Footer />
   </div>
 </template>
-<script>
-import Header from "@/components/header.vue";
-import Footer from "@/components/footer.vue";
-
-export default {
-  components: { Header, Footer },
-};
-</script>
